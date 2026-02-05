0879bc594d7b62b259611f53c78dad73a37baec3
VictorOpantaleon <ovallepantaleonvictoralejandro@gmail.com>
2026-02-04 19:08:43 -0600

    Añadidos escena de rodilla
    Modelos de 3D

--- Assets/Scripts/Lector_Distancias.cs
+++ Assets/Scripts/Lector_Distancias.cs
@@ -10,73 +10,48 @@ public class Lector_Distancias : MonoBehaviour
     public TMPro.TMP_Text Texto_Distancia;
     public TMPro.TMP_Text Texto_Estado;
     private Animator animator;
-    private Transform chest;
-    private Transform leftThigh;
+    private Transform pecho;
+    private Transform rodillaIzquierda;
     public Image Cuadrito_Fondo;
+    string EstadoActual, EstadoPasado;
+    int Repeticiones = 0;
 
-
-<<<<<<< Updated upstream
-    public void Start() //----------------------- Esta función inicializa todo
-    {
-        animator = GetComponent<Animator>(); //Ubica el cuerpo
-
-        chest = animator.GetBoneTransform(HumanBodyBones.Chest);            //Ubica el pecho
-        leftThigh = animator.GetBoneTransform(HumanBodyBones.LeftUpperLeg); //Ubica el muslo
-
-        if (chest == null || leftThigh == null)     // Si no los encuentra, avisa.
-        {
-            Debug.LogError("No se encontraron los huesos necesarios");
-        }
-=======
 
     public void Start() 
         // Validaciones iniciales de componentes - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
     {
         animator = GetComponent<Animator>();                                //Ubica el cuerpo
         pecho = animator.GetBoneTransform(HumanBodyBones.Chest);            //Ubica el pecho
-        rodillaIzquierda = animator.GetBoneTransform(HumanBodyBones.LeftLowerLeg); //Ubica el muslo
+        rodillaIzquierda = animator.GetBoneTransform(HumanBodyBones.LeftLowerLeg); //Ubica la rodilla
         
         // Si no los encuentra, avisa.
         if (pecho == null || rodillaIzquierda == null){Debug.LogError("No se encontraron los huesos necesarios");}
->>>>>>> Stashed changes
     }
 
-    public void Update() //------------------------ Esta función va midiendo e imprimiendo
+    public void Update() //------------------------ Esta funcion va midiendo e imprimiendo
     {
-<<<<<<< Updated upstream
-
-        if (chest == null || leftThigh == null) return; // Si no los encuentra, no hace na'
-
-
-        float Distancia = Vector3.Distance(chest.position, leftThigh.position);
-        Debug.Log($"Distancia muslo-pecho: {Distancia:F3}");    //Imprime la distancia en la consola.
-        Texto_Distancia.text = Distancia.ToString();            //Imprime la distancia en el texto que identificamos al principio
-                                                                //En el mundo lo asigné a "TXT_Distancia"
-=======
         if (pecho == null || rodillaIzquierda == null) return; // Si no los encuentra, no hace na'
-        Debug.DrawLine(pecho.position, rodillaIzquierda.position, Color.red);
-
-
-        // Asume posiciones y velocidades de las cosas - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
         float distanciaActual = Vector3.Distance(pecho.position, rodillaIzquierda.position);
-        //Debug.Log($"Distancia muslo-pecho: {distanciaActual:F3}");  
-        Texto_Distancia.text = distanciaActual.ToString();            //Imprime la distancia en el texto que identificamos al principio
->>>>>>> Stashed changes
 
 
-        if (Distancia <= 0.247)
-        {
-            Texto_Estado.text = "Abajo";
+        // Todo esto es la maquinita de estados.
+        if (distanciaActual <= 0.490)                   //Si la distancia pierna-pecho es corta
+        {                                               //entonces subiÃ³
+            EstadoActual = "Arriba";
+        } else if (distanciaActual >= 0.610)            //Si la distnacia pierna-pecho es larga
+        {                                               //entonces bajÃ³
+            EstadoActual = "Abajo";
+            if(EstadoPasado == "Arriba") { Repeticiones++; }  //Si repetiste ese movimiento, cuenta una repeticiÃ³n.
         }
-        else
-        {
-            if(Distancia >= 0.247)
-            {
-                Texto_Estado.text = "Arriba";
-            }
-        }
+        EstadoPasado = EstadoActual;
 
+        Debug.DrawLine(pecho.position, rodillaIzquierda.position, Color.red);
+        Texto_Distancia.text = distanciaActual.ToString();            //Imprime la distancia.
+        Texto_Estado.text = EstadoActual.ToString();                  //Imprime las reperticiones.
+        Debug.Log($"Las repeticiones hasta ahora son: {Repeticiones}");
     }
+
+
 
     
 }
