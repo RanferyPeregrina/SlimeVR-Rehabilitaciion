c4107a1bbf2f16a31c395e5b0a07595ec992bc63
Ranfery <88330135+RanferyPeregrina@users.noreply.github.com>
2026-02-05 19:57:14 -0600

    Añadidos: UI Menú Principal
    - UI de Menu Principl
    - UI de Selección de escena (2 Escenas)
    -

--- Assets/Scripts/MenuPrincipal.cs
+++ Assets/Scripts/MenuPrincipal.cs
@@ -3,13 +3,32 @@ using UnityEngine.SceneManagement;
 
 public class MenuPrincipal : MonoBehaviour
 {
+    public GameObject UIPrincipal;
+    public GameObject UISeleccion;
+
+
+    public void Start()
+    {
+        UIPrincipal.SetActive(true);
+        UISeleccion.SetActive(false);
+    }
+
     public void IniciarJuego()
     {
-        SceneManager.LoadScene(1);
+        UIPrincipal.SetActive(false);
+        UISeleccion.SetActive(true);
+    }
+
+    public void Regresar()
+    {
+        UIPrincipal.SetActive(true);
+        UISeleccion.SetActive(false);
     }
 
     public void QuitarJuego()
     {
         Application.Quit();
     }
+
+
 }
